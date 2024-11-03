e88e0a69bb814a767ee4f89d6340573a1348d20e
PJK <me@pavelkalvoda.com>
2024-11-03 19:22:08 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    # https://pypi.org/project/scanner-cli/ already taken
+    name="escl-scanner-cli",
+    version='0.1',
+    py_modules=['scanner'],
+    install_requires=[
+        'requests',
+        'xmltodict',
+        'zeroconf',
+    ],
+)
